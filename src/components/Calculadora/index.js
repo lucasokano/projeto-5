548f2e4a483eb020d96e1471e4d41f30a8917188
lucasokano <luc4sko@hotmail.com>
2021-06-11 13:42:31 -0300

    botões da calculadora adicionados

--- src/components/Calculadora/index.js
+++ src/components/Calculadora/index.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Button from '../Button';
+import Display from '../Display';
 import './index.css';
 
 function Calculadora() {
@@ -7,12 +8,24 @@ function Calculadora() {
 
   return (
     <div className="calculadora">
-        <h1>Aqui está o valor que você clicou</h1>
-        <p>{count}</p>
-        <button onClick={()=>{setcount(1)}}>1</button>
-        <button onClick={()=>{setcount(2)}}>2</button>
-        <button onClick={()=>{setcount(3)}}>3</button>
-        <Button label="oi" />
+        <Display value={100}/>
+        <Button label="AC"/>
+        <Button label="/" />
+        <Button label="7" />
+        <Button label="8" />
+        <Button label="9" />
+        <Button label="*" />
+        <Button label="4" />
+        <Button label="5" />
+        <Button label="6" />
+        <Button label="-" />
+        <Button label="1" />
+        <Button label="2" />
+        <Button label="3" />
+        <Button label="+" />
+        <Button label="0" />
+        <Button label="." />
+        <Button label="=" />
     </div>
   );
 }
